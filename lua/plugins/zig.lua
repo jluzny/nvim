bdbaaafb45f0f8819725bec529192dd7b2957670
Jiri Luzny <jiri.luzny@gmail.com>
2024-02-24 09:22:50 +0100

    Using native_snippets

--- lua/plugins/zig.lua
+++ lua/plugins/zig.lua
@@ -190,16 +190,16 @@ return {
     end,
   },
 
-  {
-    "nvimtools/none-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.builtins.formatting.zigfmt,
-      })
-    end,
-  },
+  -- {
+  --   "nvimtools/none-ls.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     local nls = require("null-ls")
+  --     opts.sources = vim.list_extend(opts.sources or {}, {
+  --       nls.builtins.formatting.zigfmt,
+  --     })
+  --   end,
+  -- },
 
   {
     "stevearc/conform.nvim",
